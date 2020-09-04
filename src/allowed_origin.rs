73be35caf6829375567e479090b79d9bf4d747c9
SCLeo <scucumber@outlook.com>
2020-09-04 18:23:35 -0400

    Update allowed origins

--- src/allowed_origin.rs
+++ src/allowed_origin.rs
@@ -3,6 +3,7 @@ pub fn is_allowed_origin(origin: &str) -> bool {
         || (origin == "https://wt.bgme.me")
         || (origin == "https://rbq.desi")
         || (origin == "https://wt.makai.city")
-        || (origin == "http://localhost:2333")
-        || (origin == "http://127.0.0.1:2333")
+        || (origin == "https://wt.0w0.bid")
+        // || (origin == "http://localhost:2333")
+        // || (origin == "http://127.0.0.1:2333")
 }
