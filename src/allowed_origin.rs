90ffb58e8a77848af3feb7480dfaabf7caf7b163
Tianyi Shi <ShiTianyi2001@outlook.com>
2021-04-27 23:11:52 +0100

    DRY rule

--- src/allowed_origin.rs
+++ src/allowed_origin.rs
@@ -1,9 +1,11 @@
 pub fn is_allowed_origin(origin: &str) -> bool {
-    (origin == "https://wt.tepis.me")
-        || (origin == "https://wt.bgme.me")
-        || (origin == "https://rbq.desi")
-        || (origin == "https://wt.makai.city")
-        || (origin == "https://wt.0w0.bid")
-        // || (origin == "http://localhost:2333")
-        // || (origin == "http://127.0.0.1:2333")
+    matches! { origin,
+          "https://wt.tepis.me"
+        | "https://wt.bgme.me"
+        | "https://rbq.desi"
+        | "https://wt.makai.city"
+        | "https://wt.0w0.bid"
+        // | "http://localhost:2333"
+        // | "http://127.0.0.1:2333"
+    }
 }
